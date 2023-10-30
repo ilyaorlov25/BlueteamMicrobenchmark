b5c8396726f25676182d8dcb1d1606b3fad5b047
ilyaorlov25 <ilya.orlov2002@yandex.ru>
2023-10-30 20:39:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/blueteammicrobenchmark/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.blueteammicrobenchmark
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.blueteammicrobenchmark.ui.theme.BlueteamMicrobenchmarkTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BlueteamMicrobenchmarkTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BlueteamMicrobenchmarkTheme {
+        Greeting("Android")
+    }
+}
